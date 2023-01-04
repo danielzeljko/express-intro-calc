d4bdf36f062ba691ff35fbd8662c8e4a8cc9b3f1
michaelpappas <pappas.michael@gmail.com>
2023-01-04 11:58:14 -0800

    calculating routes working

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /** Simple demo Express app. */
 
 const express = require("express");
+const {findMean, findMedian, findMode} = require('./stats');
+const {convertStrNums} = require("./utils")
 const app = express();
 
 // useful error class to throw
@@ -8,15 +10,29 @@ const { NotFoundError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
-
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
 
+  const numbers = convertStrNums(req.query.nums.split(","))
+  const result = findMean(numbers)
+  return res.json({operation: "mean", value: result})
+})
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
 
+  const numbers = convertStrNums(req.query.nums.split(","))
+  const result = findMedian(numbers)
+  return res.json({operation: "median", value: result})
+})
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res) {
 
+  const numbers = convertStrNums(req.query.nums.split(","))
+  const result = findMode(numbers)
+  return res.json({operation: "mode", value: result})
+})
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
--- utils.js
+++ utils.js
@@ -4,8 +4,7 @@ const { BadRequestError } = require("./expressError");
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
-  // if the conversion isn't successful, throw a BadRequestError and will
-  // be handled in your route
+  return strNums.map(num=>Number(num))
 }
 
 
